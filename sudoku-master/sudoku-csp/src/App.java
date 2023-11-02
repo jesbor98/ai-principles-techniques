5c1797615fdc627172eab62a7b687d191c0b0339
jesbor98 <jessicalborg@gmail.com>
2023-11-02 21:01:04 +0100

    fix filepath issue

--- sudoku-master/sudoku-csp/src/App.java
+++ sudoku-master/sudoku-csp/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        start("Sudoku1.txt");
+        start("sudoku-csp/Sudoku1.txt");
+        //String filePath = "C:/path/to/your/directory/Sudoku1.txt";
     }
 
     /**
